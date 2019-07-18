93d46ba0214f07c00412f3f0727eb9d4f1517688
Savanah Conrad <sibgym@yahoo.com>
2019-07-18 13:12:18 -0400

    First commit

---
+++ Powers_Table/PowersTable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Powers_Table
+{
+    class PowersTable
+    {
+        public readonly int userNumber;
+
+        public PowersTable(int userNumber)
+        {
+            this.userNumber = userNumber;
+        }
+
+        public static void TableMaker(int userNumber)
+        {
+            //creates a table showing the number from 1 to userNumber
+            //as well as the squares and cubes of those numbers
+            for (int i = 1; i <= userNumber; i++)
+            {
+                Console.Write(i + "\t\t");
+                Console.Write((i * i) + "\t\t");
+                Console.Write((i * i * i) + "\t\t");
+                Console.WriteLine("\n");
+            } 
+        }
+    }
+}
---
+++ Powers_Table/PowersTableTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Powers_Table
+{
+    public class PowersTableTest
+    {
+      public static void Main(string[] args)
+        {
+            Console.WriteLine("Learn your squares and cubes!");
+            string userChoice = "Y";
+            do
+            {
+                // Ask user for an integer value
+                Console.WriteLine("Please enter an integer:");
+
+                // Checks to make sure user input is valid
+                // If valid the program will execute
+                // If not valid, the program will promt the user for another input
+                try
+                {
+                    int userNumber = int.Parse(Console.ReadLine());
+                    Console.WriteLine("\n");
+                    Console.WriteLine("Number\t\tSquared\t\tCubed");
+                    Console.WriteLine("~~~~~~\t\t~~~~~~\t\t~~~~~~");
+                    PowersTable.TableMaker(userNumber);
+                }
+                catch
+                {
+                    Console.WriteLine("\nInvalid. Please try again.\n");
+                    continue;
+                }
+
+                Console.WriteLine("Would you like to continue? Y/N.");
+                userChoice = Console.ReadLine().ToUpper();
+            } while (userChoice == "Y");
+            Console.WriteLine("Thanks for making a table!");
+        }
+    }
+}
